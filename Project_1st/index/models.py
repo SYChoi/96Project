11dc3506162ffc03f4fba0d991db5d04892768ba
SYChoi <sychoi1996@naver.com>
2019-02-17 22:42:47 +0900

    index/models.py event class completed

--- Project_1st/index/models.py
+++ Project_1st/index/models.py
@@ -2,18 +2,29 @@ from django.db import models
 from account.models import Account
 
 class Event(models.Model):
-    eventID = models.IntegerField(primary_key=True)
-    eventname = models.CharField(max_length=30)
+    eventname = models.CharField(db_index=True, max_length=30, primary_key=True)
     user = models.ForeignKey('account.Account', on_delete=models.CASCADE)
     date = models.DateField()
     time = models.TimeField()
     place = models.CharField(max_length=50)
     detail = models.TextField()
 
-    def register(self, form):
+    def register(self, form, request):
         eventname = form['eventname'].data
         date = form['date'].data
         time = form['time'].data
         place = form['place'].data
         detail = form['detail'].data
+        try:
+            new_event = Event.objects.create(
+                eventname = eventname,
+                user = request.user, 
+                date = date,
+                time = time,
+                place = place,
+                detail = detail,
+            )
+        except:
+            return False
+        return True
 
