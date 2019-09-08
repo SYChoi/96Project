1f0038de7c49b09a1d07aef408b8695f6cb2b85d
SYChoi <sychoi1996@naver.com>
2019-09-08 14:15:31 +0900

    project_2nd tab menu made

--- Project_2nd/Hyunjin/Hyunjin/settings.py
+++ Project_2nd/Hyunjin/Hyunjin/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+'''
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+]
+'''
