c9a667afa250c18d48e6d4ef6321245008924178
CSY <sychoi1996@naver.com>
2020-07-27 14:49:10 +0900

    html files created && url setting / menu modified

--- Project_2nd/Hyunjin/Main/urls.py
+++ Project_2nd/Hyunjin/Main/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+	path('', views.index),
+	path('notice/', views.notice),
+	path('antirust/', views.antirust),
+	path('equip/', views.equip),
+	path('consulting/', views.consulting),
+	path('online_consult/', views.online_consult),
+	path('raw_material/', views.raw_mat),
+	path('industrial_part/', views.indust_part),
+	path('download/', views.download),
+	path('engine_material/', views.engine_mat),
 ]
--- Project_2nd/Hyunjin/Main/views.py
+++ Project_2nd/Hyunjin/Main/views.py
@@ -1,4 +1,31 @@
 from django.shortcuts import render, redirect
 
 def index(request):
-    return render(request, 'main.html',)
+	return render(request, 'main.html',)
+
+def notice(request):
+	return render(request, 'notice.html')
+
+def antirust(request):
+	return render(request, 'antirust_agent.html')
+
+def equip(request):
+	return render(request, 'equipment_craft.html')
+
+def consulting(request):
+	return render(request, 'consulting.html')
+
+def online_consult(request):
+	return render(request, 'online_consult.html')
+
+def raw_mat(request):
+	return render(request, 'raw_material.html')
+
+def indust_part(request):
+	return render(request, 'industrial_part.html')
+
+def download(request):
+	return render(request, 'download.html')
+
+def engine_mat(request):
+	return render(request, 'engine_material.html')
