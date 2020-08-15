9d9c29c7dbb2ba32ce7ce97db9fc2c6ced05ed13
CSY <sychoi1996@naver.com>
2020-08-15 22:08:03 +0900

    notice post created

--- Project_2nd/Hyunjin/Main/models.py
+++ Project_2nd/Hyunjin/Main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+	name = models.CharField(max_length = 100, blank = False)
+	date = models.DateField(auto_now_add=True)
+	content = models.CharField(max_length = 10000, blank = True)
--- Project_2nd/Hyunjin/Main/views.py
+++ Project_2nd/Hyunjin/Main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from Main.models import Post
+#from Main.forms import PostForm
 
 def index(request):
 	return render(request, 'main.html')
@@ -10,8 +12,20 @@ def contact(request):
 	return render(request, 'contact.html')
 
 def notice(request):
-	return render(request, 'notice.html')
-
+	posts = Post.objects.all()
+	return render(request, 'notice.html', {'posts': posts})
+"""
+def notice_create(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=True)
+			return notice(request)
+	else:
+		form = PostForm
+		pw = "123"
+		return render(request, 'notice_create.html', {'form': form, 'pw': pw})
+"""
 def antirust(request):
 	return render(request, 'antirust_agent.html')
 
